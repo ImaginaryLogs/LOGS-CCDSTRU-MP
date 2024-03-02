b13cf8d132bd89229584361fd8362ff8c105adbf
Complex MacWood <logarithmicpresence@gmail.com>
2024-03-03 02:28:56 +0800

    Initialized code for thereExistFunction

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 
 #include <stdio.h>
 #include <stdbool.h>
-#define MAXSIZE 36
+#include <stdlib.h>
+#include <math.h>
+#define MAXSIZE 72
 
 typedef char String15[16];
 typedef int coord[2];
 
 // A general struct for a set of 1D
 struct set1D {
-    coord D1Array[MAXSIZE*MAXSIZE];
+    coord D1Array[MAXSIZE];
     int length;
     int maxSize;
 };
@@ -20,12 +22,17 @@ struct set2D {
     int maxSize;
 };
 
+struct set3D {
+    struct set2D D3Array[MAXSIZE];
+    int height;
+    int maxSize;
+};
 
 // #=====| Functions of Coordinates |======#
 /**
  * @brief Source := Destination
  * @note Copies coordinate from Source to Destination.
- * @param Destination Coordinate you want to copy.
+ * @param Destination Coordinate you want to assign.
  * @param Source  Coordinate you want copy from.
  */
 void coordCopy(coord Destination, coord Source){
@@ -49,7 +56,7 @@ void coordPrint(coord Coordinate){
  * @retval 1 if Equal
  * @retval 0 if Not Equal
  */
-bool cordEqual(coord A, coord B){
+bool coordEqual(coord A, coord B){
     return A[0] == B[0] && A[1] == B[1];
 }
 
@@ -59,7 +66,7 @@ bool cordEqual(coord A, coord B){
  * @param set[]: The 1D Array you want to print.
  * @param size: Size of 1D Array
  */
-void cord1DPrint(coord set[], int size){
+void coord1DPrint(coord set[], int size){
     int i;
     for(i = 0; i < size; i++)
         coordPrint(set[i]);
@@ -73,7 +80,7 @@ void cord1DPrint(coord set[], int size){
  * @param *dest: Destination
  * @param sizeDest: Size of Destination
  */
-void set1DCopyFromCord1D(coord *srce, coord *dest, int sizeDest){
+void set1DCopyFromCoord1D(coord *srce, coord *dest, int sizeDest){
     int i;
     for(i = 0; i < sizeDest; i++) {
         coordCopy(srce[i], dest[i]);
@@ -105,6 +112,7 @@ void set1DCopy(struct set1D *setDEST, struct set1D *setSRCE){
     }
 }
 
+
 /**
  * @brief is key ∈ Source True?
  * @note (Struct1D) Searches up setSource (Struct of 1D set) for the key. Location of key is in output - returns (-1, -1) if not found.
@@ -115,17 +123,75 @@ void set1DCopy(struct set1D *setDEST, struct set1D *setSRCE){
  * @retval 1 if key ∈ Source
  * @retval 0 if key ∉ Source
  */
-bool set1DSearch(coord key, struct set1D *setSource, int *location){
+bool set1DSearch0Dkey(coord key, struct set1D *setSource, int *location){
     int j;
     *location = -1;
     for (j = 0; j < setSource->length; j++){
-        if (cordEqual(key, setSource->D1Array[j])){
+        if (coordEqual(key, setSource->D1Array[j])){
             *location = j;
             return true;
         }
     }
     
     return false;
+}
+
+bool set1DEqual(struct set1D *set1DSRCE, struct set1D *set1DDEST){
+    bool isArrayMembersEqual = true;
+    int i;
+    int size = (set1DSRCE->length >= set1DDEST->length) ? set1DSRCE->length : set1DDEST->length;
+    for(i = 0; i < size && isArrayMembersEqual; i++){
+        isArrayMembersEqual = isArrayMembersEqual && coordEqual(set1DSRCE->D1Array[i], set1DDEST->D1Array[i]);
+    }
+    return isArrayMembersEqual && set1DSRCE->length == set1DDEST->length && set1DSRCE->maxSize == set1DDEST->maxSize;
+}
+
+/**
+ * @brief setSRCE = P(setDEST)
+ * @note Computes the power set of setDEST
+ * @param *setDEST: A set1D struct you want to get the power set from
+ * @param *setSRCE: A set2D struct the power set will copy to
+ */
+void powerSet1D(struct set1D *setDEST, struct set2D *setSRCE){
+    int i, j, elements;
+    int powerSetSize = pow(2, setDEST->length);
+    setSRCE->maxSize = powerSetSize;
+    setSRCE->width = powerSetSize;
+    for(i = 0; i < powerSetSize; i++){
+        elements = 0;
+        for(j = 0; j < setDEST->length; j++){
+            if(i & (1 << j)){
+                coordPrint(setDEST->D1Array[j]);
+                coordCopy(setSRCE->D2Array[i].D1Array[elements], setDEST->D1Array[j]);
+                elements++;
+            }
+        }
+        setSRCE->D2Array[i].length = elements;
+        printf("\n");
+    }
+
+};
+
+/**
+ * @brief sorts set1D  
+ * @param  *setDEST: A set1D struct you want to help copy from
+ */
+void sortSelection1DSet(struct set1D *setDEST){
+    int i, j, max;
+    coord temp = {0};
+    for(i = 0; i < setDEST->maxSize - 1; i++){
+        max = i;
+        for(j = i; j < setDEST->maxSize; j++){
+            if(setDEST->D1Array[j] > setDEST->D1Array[max])
+                max = j;
+            
+        }
+        if (max != i){
+            coordCopy(temp, setDEST->D1Array[max]);
+            coordCopy(setDEST->D1Array[max], setDEST->D1Array[i]);
+            coordCopy(setDEST->D1Array[i], temp);
+        }
+    }
 }
 
 // #=====| Functions of STRUCT 2D Sets |======#
@@ -138,7 +204,7 @@ void set2DPrint(struct set2D *set){
     int row, col;
     printf("Printing 2D: \n");
     for(row = 0; row < width; row++){
-        printf("\t%2d: ", row);
+        printf("\t%02d: ", row);
         set1DPrint(&(set->D2Array[row]));
     }
 }
@@ -165,13 +231,13 @@ void set2DCopy(struct set2D *setSRCE, struct set2D *setDEST){
  * @retval 1 if key ∈ Source
  * @retval 0 if key ∉ Source
  */
-bool set2DSearch(coord key, struct set2D *setSource, coord location){
+bool set2DSearch0Dkey(coord key, struct set2D *setSource, coord location){
     location[0] = -1;
     location[1] = -1;
     int i, j;
     for (i = 0; i < setSource->width; i++){
         for (j = 0; j < setSource->D2Array[i].length; j++){
-            if (cordEqual(key, setSource->D2Array[i].D1Array[j])){
+            if (coordEqual(key, setSource->D2Array[i].D1Array[j])){
                 location[0] = i;
                 location[1] = j;
                 return true;
@@ -181,7 +247,47 @@ bool set2DSearch(coord key, struct set2D *setSource, coord location){
     return false;
 }
 
+bool set2DSearch1Dkey(struct set1D *key, struct set2D *setSource, coord location){
+    int temp;
+    int i, j;
+    int isSearch0DSuccess;
+    int isFound = false;
+    printf("Searching: Set with size %d\n", key->length);
+    set1DPrint(key);
+    printf("inside the set\n");
+    set2DPrint(setSource);
+    for (i = 0; i < key->length && !isFound; j++){
+        isSearch0DSuccess = true;
+        for (j = 0; j < setSource->D2Array[i].length && isSearch0DSuccess; j++){
+            isSearch0DSuccess = set1DEqual(key, &setSource->D2Array[j]);
+        }
+        printf("E: %d\n", j == setSource->D2Array[i].length);
+        if (j == setSource->D2Array[i].length){
+            isFound = true;
+        }
+    }
+    return isFound; 
+}
+
 // #=====| Other functions |=================#
+
+bool thereExistCondition1(struct set2D *powerSetofSomeSet, struct set2D *setP){
+    bool isExist = false;
+    int XSizeGreater0;
+    int XisInP;
+    coord location = {0};
+    int i, j;
+    for (i = 0; i < powerSetofSomeSet->width && !isExist; i++){
+        XSizeGreater0 = powerSetofSomeSet->D2Array[i].length > 0;
+        XisInP = set2DSearch1Dkey(&powerSetofSomeSet->D2Array[i], setP, location);
+        printf("X exist %d and is X in P %d\n", XSizeGreater0, XisInP);
+        if(XSizeGreater0 && XisInP){
+            isExist = true;
+        }
+    }
+    return isExist;
+}
+
 /**
  * @brief Initialize (set2D) Set S based on MP Specs  
  * @param *setS: A set2D struct you will assign as S
@@ -196,11 +302,11 @@ void initializeS(struct set2D *setS){
     };
 
     for (i = 0; i < 3; i++){
-        set1DCopyFromCord1D(setS->D2Array[i].D1Array, Quad[i], 5);
+        set1DCopyFromCoord1D(setS->D2Array[i].D1Array, Quad[i], 5);
         setS->D2Array[i].length = 5;
     }
     // i = 3 after loop
-    set1DCopyFromCord1D(setS->D2Array[i].D1Array, Quad[i], 6);
+    set1DCopyFromCoord1D(setS->D2Array[i].D1Array, Quad[i], 6);
     setS->D2Array[i].length = 6;
     setS->width = 4;
     setS->maxSize = 6;
@@ -218,7 +324,7 @@ void initializeP(struct set2D *setP){
     };
 
     for (i = 0; i < 2; i++){
-        set1DCopyFromCord1D(setP->D2Array[i].D1Array, Duo[i], 2);
+        set1DCopyFromCoord1D(setP->D2Array[i].D1Array, Duo[i], 2);
         setP->D2Array[i].length = 2;
     }
     setP->width = 2;
@@ -247,14 +353,14 @@ int main(int argc, char const *argv[])
 
     bool setV[2] = {true, false};
 
-    struct set2D setC, setF, setS, setP;
+    struct set2D setC, setF, setS, setP, setPowerSetS;
 
     coord output = {0};
     coord key = {4, 6};
 
     initializeS(&setS);
     initializeP(&setP);
-    set2DSearch(key, &setS, output);
+    set2DSearch0Dkey(key, &setS, output);
     coordPrint(output);
     printf("\n");
     // System Variables
@@ -269,7 +375,10 @@ int main(int argc, char const *argv[])
     coord F3; 
 
     set2DPrint(&setS);
-
+    set2DPrint(&setPowerSetS);
+    powerSet1D(&setS.D2Array[0], &setPowerSetS);
+    set2DPrint(&setPowerSetS);
+    printf("%d", thereExistCondition1(&setPowerSetS, &setP));
     GameOver(&over, &next);
     return 0;
 }
