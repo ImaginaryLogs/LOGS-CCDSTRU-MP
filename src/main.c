7787b9fb3280d6e0b6db4eaf18ab78d3794cf395
Venless <160856445+Venless@users.noreply.github.com>
2024-03-31 17:40:44 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,11 +1,14 @@
 	
 #include "funcsSystem.h"
 
-int main(){
+int main()
+{
     struct set1D F, F1, F2, F3, C, C1, C2;
     struct set2D setS, setP;
+	
     int i = 0;
     int a, b;
+	
     set1DClear(&F);
     set1DClear(&F1);
     set1DClear(&F2);
@@ -19,6 +22,7 @@ int main(){
     initializeP(&setP);
     updateF3(&F, &F1, &F2, &F3);
     // System Variables
+	
     bool good = false;
     bool next = false;	
     bool over = false;
@@ -28,24 +32,31 @@ int main(){
 		printf("Welcome To The MP Project\n");
 		printf("    [0]: Play the game\n");
 		printf("    [1]: Exit program\n");
-        
 		printf("Input: ");
 		scanf("%d", &a);
-		if (a == 1) {
+		
+		if (a == 1) 
+		{
 			b == 1;
 			printf("Game Ended.\n");
-		} else if (a == 0) {
-            while (!over) {
-                RepeatGetCoord2(&F1, &F2, &F3, &C1, &C2, input, &next);
-                updateOver(&F3, &C1, &C2, &over, &setP);
-                NextPlayerMove1(input, &F, &F1, &F2, &F3, over, &good, next, &setS, &C1, &C2, i);
-                updateOver(&F3, &C1, &C2, &over, &setP);
-                GameOver2(&over, &next, &C1, &C2, &setP, &F1, &F2, &F3, input);
-                i++;
-            }
-		} else {
+		} 
+		
+		else if (a == 0) 
+		{
+			while (!over) 
+			{
+	                RepeatGetCoord2(&F1, &F2, &F3, &C1, &C2, input, &next);
+	                updateOver(&F3, &C1, &C2, &over, &setP);
+	                NextPlayerMove1(input, &F, &F1, &F2, &F3, over, &good, next, &setS, &C1, &C2, i);
+	                updateOver(&F3, &C1, &C2, &over, &setP);
+	                GameOver2(&over, &next, &C1, &C2, &setP, &F1, &F2, &F3, input);
+	                i++;
+	          	}
+			
+		} else
+			{
 			printf("Invalid Number. Please Enter 0 or 1");
-		}
+			}
 	}
 	while (b = 0);
 
